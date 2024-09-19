b09fcf6154507e5727a42ab515b33b95a470b2e3
ziko278 <odekeziko@gmail.com>
2024-09-19 13:02:44 +0100

    updated

--- workings/settings.py
+++ workings/settings.py
@@ -134,11 +134,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'odekeziko@gmail.com'
-EMAIL_HOST_PASSWORD = 'syqcunjokqzrqlre'
+# EMAIL_HOST_USER = 'odekeziko@gmail.com'
+# EMAIL_HOST_PASSWORD = 'syqcunjokqzrqlre'
 
-# EMAIL_HOST_USER = 'andersonfisherr@gmail.com'
-# EMAIL_HOST_PASSWORD = 'bndmrymbnvyfhluv'
+EMAIL_HOST_USER = 'andersonfisherr@gmail.com'
+EMAIL_HOST_PASSWORD = 'bndmrymbnvyfhluv'
 
 # EMAIL_HOST_USER = 'andrebianchi56@gmail.com'
 # EMAIL_HOST_PASSWORD = 'tzjghooyrmgaczub'
